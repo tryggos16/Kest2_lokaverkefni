d2095511f258f37d1e4372f42a9918e4f52a1611
Tryggvi Þór Bogason <101139768+tryggos16@users.noreply.github.com>
2023-02-25 21:28:18 +0000

    Add files via upload

---
+++ powershell_code/create_and_add_to_group.ps1
@@ -0,0 +1,9 @@
+﻿$gogn = Import-Csv C:\Users\Tryggvi\Documents\kest_usersetup_csv_file.csv
+
+foreach($data in $gogn) { 
+    New-LocalUser -name $data.username -FullName $data.fullname -Password ($data.Password | ConvertTo-SecureString -AsPlainText -Force)
+    Add-LocalGroupMember -Group $data.group -Member $data.username
+    Add-LocalGroupMember -Group Allir -Member $data.username
+    Add-LocalGroupMember -Group Users -Member $data.username
+}
+ 
