b9c2315cb4523394a234b4c56a6915de77cf5a9b
Tryggvi Þór Bogason <101139768+tryggos16@users.noreply.github.com>
2023-02-25 23:21:45 +0000

    Add files via upload

---
+++ powershell_code/Creating_dir_GroupFolder_files_Access.ps1
@@ -0,0 +1,42 @@
+﻿$gogn = Import-Csv C:\Users\Tryggvi\Documents\Gogn_and_group_dir.csv
+$access = Import-Csv C:\Users\Tryggvi\Documents\AllowAccessesGroupFolder.csv
+
+# Creating the "Gogn" dir
+New-Item -Path C: -Name Gogn -ItemType Directory
+
+
+# creates the new folders by the Group and stores them in the "Gogn" dir
+# then creates text files inside the Group folders with a value
+
+foreach($data in $gogn) {
+    $path = $data.GroupFolder
+    New-Item -Path C:\Windows\System32\Gogn -Name $data.GroupFolder -ItemType Directory
+    New-Item -Path C:\Windows\System32\Gogn\$path -Name $data.fileName -ItemType 'file' -Value $data.value
+    
+}
+ 
+
+# Grants access to the folders for those in the apropeit Group
+
+foreach($data in $access) {
+    $folderPath = $data.FolderPath
+
+    $group = $data.Group
+
+    $accessesType = "FullControl"
+
+    $allowOrDeny = "Allow"
+
+    $argList = $group,$accessesType,$allowOrDeny
+
+    $acl = Get-Acl $folderPath
+
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule -ArgumentList $argList
+
+    $acl.setAccessRule($accessRule)
+
+    $acl | Set-Acl $folderPath
+    
+    
+}
+
